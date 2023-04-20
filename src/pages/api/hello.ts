a193683e5b817c0ff0ae25f39e8ea529d07076ac
Yosuke Kimoto <yosuke.kmt@gmail.com>
2023-04-20 12:21:31 +0800

    compilable

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,52 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import * as ethers from "ethers";
+import * as fs from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as path from "path";
+import * as solc from "solc";
 
-type Data = {
-  name: string
-}
+const infuraApiKey = process.env.INFURA_API_KEY || "";
+const walletPrivateKey = process.env.WALLET_PRIVATE_KEY || "";
 
-export default function handler(
+const findImports = (importPath: string) => {
+  if (importPath.startsWith("@openzeppelin")) {
+    const openZeppelinPath = path.resolve("node_modules", importPath);
+    return { contents: fs.readFileSync(openZeppelinPath, "utf8") };
+  } else {
+    return { error: `File not found: ${importPath}` };
+  }
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const source = req.body.contract;
+  const contractNameRegex = /contract\s+(\w+)\s*([^{]*)\{/;
+  const match = contractNameRegex.exec(source);
+  const contractName = match && match[1] ? match[1] : "";
+  const sourceName = `${contractName}.sol`;
+
+  let input: any = {
+    language: "Solidity",
+    sources: {},
+    settings: { outputSelection: { "*": { "*": ["*"] } } },
+  };
+  input.sources[sourceName] = { content: source };
+  const output = JSON.parse(
+    solc.compile(JSON.stringify(input), { import: findImports })
+  );
+  const bytecode =
+    output.contracts[sourceName][contractName].evm.bytecode.object;
+  const abi = output.contracts[sourceName][contractName].abi;
+  const prorider = new ethers.providers.JsonRpcProvider(
+    `https://mainnet.infura.io/v3/${infuraApiKey}`
+  );
+  const signerWallet = new ethers.Wallet(walletPrivateKey, prorider);
+  const factory = new ethers.ContractFactory(abi, bytecode, signerWallet);
+  const deployTransaction = factory.getDeployTransaction();
+  const gasLimit = await signerWallet.estimateGas(deployTransaction);
+  const gasPrice = await prorider.getGasPrice();
+  const transactionFee = gasPrice.mul(gasLimit);
+
+  res.status(200).json({ gasLimit, gasPrice, transactionFee });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,243 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+  InputGroup,
+  Textarea,
+} from "@chakra-ui/react";
+import { BigNumber, ethers } from "ethers";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const Header = () => {
+  return (
+    <Box h="100%">
+      <Container maxWidth="6xl" h="100%">
+        <Flex align="center" h="100%">
+          <Heading as="h1" fontSize="lg" fontWeight="light">
+            ethereum Deploy Cost calculator
+          </Heading>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+const Body = () => {
+  const [contract, setContract] =
+    useState(`// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SimpleERC721 is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SimpleERC721", "SE721") {}
+
+    function mint(address to) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(to, newItemId);
+
+        return newItemId;
+    }
+}
+  `);
+  const [gasLimitWei, setGasLimitWei] = useState("");
+  const [gasLimitGwei, setGasLimitGwei] = useState("");
+  const [gasLimitEther, setGasLimitEther] = useState("");
+  const [gasPriceWei, setGasPriceWei] = useState("");
+  const [gasPriceGwei, setGasPriceGwei] = useState("");
+  const [gasPriceEther, setGasPriceEther] = useState("");
+  const [transactionFeeWei, setTransactionFeeWei] = useState("");
+  const [transactionFeeGwei, setTransactionFeeGwei] = useState("");
+  const [transactionFeeEther, setTransactionFeeEther] = useState("");
+
+  const clickEstimate = async () => {
+    try {
+      const resp = await fetch("/api/hello", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ contract: contract }),
+      });
+
+      //res.status(200).json({ gasLimit, gasPrice, gasFee });
+      if (resp) {
+        const data = await resp.json();
+        if (data.gasLimit) {
+          const gasLimit = BigNumber.from(data.gasLimit);
+          setGasLimitWei(gasLimit.toString());
+          setGasLimitGwei(
+            ethers.utils.formatUnits(gasLimit, "gwei").toString()
+          );
+          setGasLimitEther(
+            ethers.utils.formatUnits(gasLimit, "ether").toString()
+          );
+        }
+        if (data.gasPrice) {
+          const gasPrice = BigNumber.from(data.gasPrice);
+          setGasPriceWei(gasPrice.toString());
+          setGasPriceGwei(
+            ethers.utils.formatUnits(gasPrice, "gwei").toString()
+          );
+          setGasPriceEther(
+            ethers.utils.formatUnits(gasPrice, "ether").toString()
+          );
+        }
+        if (data.transactionFee) {
+          const transactionFee = BigNumber.from(data.transactionFee);
+          setTransactionFeeWei(transactionFee.toString());
+          setTransactionFeeGwei(
+            ethers.utils.formatUnits(transactionFee, "gwei").toString()
+          );
+          setTransactionFeeEther(
+            ethers.utils.formatUnits(transactionFee, "ether").toString()
+          );
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+    }
+  };
+
+  return (
+    <Box h="100%">
+      <Container maxWidth="6xl" h="100%">
+        <Grid templateColumns="repeat(12, 1fr)" gap={4} py={12}>
+          <GridItem colSpan={{ base: 12, md: 6 }}>
+            <FormControl h="calc(100% - 40px)">
+              <FormLabel fontSize="sm">Contract</FormLabel>
+              <Textarea
+                value={contract}
+                h="calc(100% - 21px)"
+                onChange={(evt) => setContract(evt.target.value)}
+              ></Textarea>
+            </FormControl>
+            <Button mt={4} w="100%" onClick={clickEstimate}>
+              Estimate
+            </Button>
+          </GridItem>
+          <GridItem colSpan={{ base: 12, md: 6 }}>
+            <Heading as="h3" fontSize="xl" fontWeight="light">
+              Gas Limit (Estimated)
+            </Heading>
+            <FormControl mt={2}>
+              <FormLabel fontSize="sm">Wei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasLimitWei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Gwei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasLimitGwei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Ether</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasLimitEther} />
+              </InputGroup>
+            </FormControl>
+            <Heading as="h3" fontSize="xl" fontWeight="light" mt={8}>
+              Current Gas Price (Estimated)
+            </Heading>
+            <FormControl mt={2}>
+              <FormLabel fontSize="sm">Wei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasPriceWei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Gwei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasPriceGwei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Ether</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={gasPriceEther} />
+              </InputGroup>
+            </FormControl>
+            <Heading as="h3" fontSize="xl" fontWeight="light" mt={8}>
+              Total Transaction Fee (Estimated)
+            </Heading>
+            <FormControl mt={2}>
+              <FormLabel fontSize="sm">Wei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={transactionFeeWei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Gwei</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={transactionFeeGwei} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel fontSize="sm">Ether</FormLabel>
+              <InputGroup size="lg">
+                <Input type="number" value={transactionFeeEther} />
+              </InputGroup>
+            </FormControl>
+          </GridItem>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+const Footer = () => {
+  return (
+    <Box h="100%">
+      <Container maxWidth="6xl" h="100%">
+        <Flex align="center" h="100%" justify="center">
+          <Heading as="h1" fontSize="lg" fontWeight="light">
+            Created by yosuke.kmt@gmail.com
+          </Heading>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>ethereum Deploy Cost calculator</title>
         <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Box h="100vh">
+        <Box as="header" h="48px" bg="gray.100">
+          <Header />
+        </Box>
+        <Box as="main">
+          <Body />
+        </Box>
+        <Box as="footer" h="48px" bg="gray.100">
+          <Footer />
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
